7c143c3b34ffd6f20bddcdfbe685364bc5760e7f
Drissiy <unesdrissi2@gmail.com>
2024-08-08 08:40:29 +0100

    deploy

--- src/index.js
+++ src/index.js
@@ -6,11 +6,10 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const basename = process.env.NODE_ENV === 'production' ? '/portfolio' : '/';
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter basename='/portfolio'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
